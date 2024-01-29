e1eb8560683ae70ff0819d006e95d263d81a61c0
shubhamtharkar <shubhamtharkar2004@gmail.com>
2024-01-29 23:09:33 +0530

    rejouice

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function cursoreffect(){
+    var page1Content = document.getElementById("page1-content");
+var cursor = document.getElementById("cursor");
+
+page1Content.addEventListener("mousemove", function(dets){
+    gsap.to(cursor,{
+        x:dets.x,
+        y:dets.y
+    })
+})
+
+page1Content.addEventListener("mouseenter", function() {
+    gsap.to(cursor,{
+        scale:1,
+        opacity:1
+    })
+})
+
+page1Content.addEventListener("mouseleave", function() {
+    gsap.to(cursor,{
+        scale:0,
+        opacity:0
+    })
+})
+}
+cursoreffect();
